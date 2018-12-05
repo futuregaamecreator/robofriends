022fb78111b46814a3cfde8c8291ea122999a2ea
George Harrison <futuregamecreator@gmail.com>
2018-12-05 14:11:35 -0500

    stopped at the 13 min mark
    stopped at the 13th mark

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = () =>{
+    return(
+
+        <input
+            className = 'pa3 ba b--green bg-lightest-blue'
+            type='search'
+            placeholder='search robots'
+        />
+    )
+}
+
+export default SearchBox;
